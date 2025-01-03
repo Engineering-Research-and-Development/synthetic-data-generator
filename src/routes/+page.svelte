fe12d816f4c0d256dcecf6f8b2e77225fb4d4a98
Fabio Cassano <fabio.cassano@eng.it>
2025-01-03 11:16:59 +0100

    Improved home page, upload and structures

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,114 @@
-<h1>Welcome to the Syntetic data generator</h1>
-<a href="upload"> Upload a CSV</a>
-<br><br>
-<a href="newdataset"> Create a new dataset </a>
+<script>
+    import { Img, Button, Modal, Fileupload, Spinner } from 'flowbite-svelte';
+    import { goto } from "$app/navigation";
+    import Papa from 'papaparse';
+
+    const createEndpoint = '/create';
+    let showPopup = false;
+    let uploadedFile = File;
+    let isSubmitting = false;
+
+    function handleFileUpload(event) {
+        const file = event.target.files[0];
+
+        if (file.size > 10 * 1024 * 1024) {
+            alert('File size exceeds 10 MB. Please upload a smaller file.');
+            return;
+        }
+
+        uploadedFile = file;
+    }
+
+    function submitForm(event) {
+        event.preventDefault();
+        isSubmitting = true;
+
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            Papa.parse(reader.result, {
+                header: true,
+                skipEmptyLines: true,
+                complete: (result) => {
+                    sessionStorage.setItem('userFile', JSON.stringify(result.data));
+                    isSubmitting = false;
+                    goto('/edit');
+                },
+            });
+        };
+
+        reader.readAsText(uploadedFile);
+    }
+
+</script>
+
+
+<div class="h-screen w-screen bg-cover bg-center flex items-center justify-center relative"
+     style="background-image: url('/pexels-markusspiske-1089438.jpg');">
+    <div class="text-center relative z-10">
+        <Img src="ENG-LOGO-LIGHT.png" alt="ENG logo" size="max-w-xs" alignment="mx-auto"/>
+        <h1 class="text-white text-4xl md:text-6xl font-bold mb-6">Data Synthetic Generator</h1>
+        <div class="flex justify-center space-x-4">
+            <Button
+                    on:click={() => (showPopup = true)}
+                    class="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition">
+                Enhance Existing dataset
+            </Button>
+            <Button
+                    href="{createEndpoint}"
+                    class="px-6 py-3 bg-gray-600 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 transition">
+                Create a dataset from scratch
+            </Button>
+        </div>
+    </div>
+
+    {#if isSubmitting}
+        <div class="absolute inset-0 bg-black opacity-90 flex items-center justify-center z-50">
+            <div class="flex flex-col items-center">
+                <Spinner size="xl"/>
+                <span class="text-white mt-4">Uploading...</span>
+            </div>
+        </div>
+    {/if}
+</div>
+
+<Modal bind:open={showPopup} size="md" autoclose outsideclose>
+    <div slot="header">Upload CSV</div>
+
+    <div class="p-4">
+        <form on:submit|preventDefault={submitForm}>
+            <div class="mb-4">
+                <label
+                        for="csvFile"
+                        class="block text-sm font-medium text-gray-700 mb-2"
+                >
+                    Choose a CSV file
+                </label>
+                <Fileupload
+                        id="csvFile"
+                        type="file"
+                        accept=".csv"
+                        on:change={handleFileUpload}
+                        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none focus:ring focus:border-blue-300"
+                        required
+                />
+            </div>
+            <div class="flex justify-end">
+                <Button
+                        type="button"
+                        on:click={() => (showPopup = false)}
+                        class="text-gray-500 bg-white border border-gray-300 rounded-md px-4 py-2 mr-2 hover:bg-gray-100"
+                >
+                    Cancel
+                </Button>
+                <Button
+                        type="submit"
+                        on:click={submitForm}
+                        class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                >
+                    Upload
+                </Button>
+            </div>
+        </form>
+    </div>
+</Modal>
--- src/routes/edit/+page.svelte
+++ src/routes/edit/+page.svelte
@@ -1,31 +1,104 @@
 <script>
-	import {goto} from '$app/navigation';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Checkbox,
+		Button, Modal
+	} from 'flowbite-svelte';
 
-	let csvContent = sessionStorage.getItem('userFile');
-	let lines = csvContent.split('\n').slice(0, 10);
-	let headers = lines[0].split(',');
-	let selectedColumns = [];
+	import { onMount } from 'svelte';
+	import {ExclamationCircleOutline} from "flowbite-svelte-icons";
+	import {goto} from "$app/navigation";
 
-	function handleColumnSelection(event) {
-		selectedColumns = Array.from(event.target.selectedOptions, option => option.value);
+	let tableData = [String];
+	let headers = [String];
+	let selectedColumns = [String];
+	let showCancelPopup = false;
+
+	onMount(() => {
+		const savedData = sessionStorage.getItem('userFile');
+		if (savedData) {
+			const parsedData = JSON.parse(savedData);
+			if (parsedData.length > 0) {
+				headers = Object.keys(parsedData[0]);
+				tableData = parsedData;
+			}
+		} else {
+			console.error("No data found in sessionStorage.");
+		}
+	});
+
+	function toggleColumn(column) {
+		if (selectedColumns.includes(column)) {
+			selectedColumns = selectedColumns.filter(name => name !== column); // Remove column
+		} else {
+			selectedColumns = [...selectedColumns, column]; // Add column
+		}
 	}
 
-	function goToSendPage() {
-		goto('/send');
+	function submitForm(){
+		sessionStorage.setItem('selectedColumns', JSON.stringify(selectedColumns))
 	}
+	function cancelProcedure(){
+		sessionStorage.removeItem('csvFile')
+		goto("/")
+	}
+
 </script>
 
-<main>
-	<h1>Preview CSV File</h1>
-	<p>First 10 lines:</p>
-	<pre>{lines.join('\n')}</pre>
+<h1 class="flex justify-center text-2xl font-bold my-4">Uploaded CSV Data</h1>
+<div class="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+	<form on:submit|preventDefault={submitForm}>
+		<Table class="w-3/4 text-gray-500 dark:text-gray-400" shadow>
+			<TableHead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+				{#each headers as header}
+					<TableHeadCell>
+						<div class="flex items-center space-x-2">
+							<Checkbox
+									checked={selectedColumns.includes(header)}
+									on:change={() => toggleColumn(header)}
+							/>
+							<span>{header}</span>
+						</div>
+					</TableHeadCell>
+				{/each}
+			</TableHead>
+			<TableBody tableBodyClass="divide-y">
+				{#each tableData.slice(0, 10) as row}
+					<TableBodyRow class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						{#each headers as header}
+							<TableBodyCell>
+								{row[header]}
+							</TableBodyCell>
+						{/each}
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
 
-	<h2>Select Columns</h2>
-	<select multiple on:change={handleColumnSelection}>
-		{#each headers as header}
-			<option value={header}>{header}</option>
-		{/each}
-	</select>
-
-	<button on:click={goToSendPage}>Next</button>
-</main>
+		<Button
+				type="submit"
+				class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+		>
+			Next
+		</Button>
+		<Button
+				on:click={() => (showCancelPopup = true)}
+				class="text-gray-500 bg-white border border-gray-300 rounded-md px-4 py-2 mr-2 hover:bg-gray-100"
+		>
+			Cancel
+		</Button>
+		<Modal bind:open={showCancelPopup} size="xs" autoclose>
+			<div class="text-center">
+				<ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
+				<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">This will cause the data loss</h3>
+				<Button color="red" class="me-2" on:click={cancelProcedure}>Yes, I'm sure</Button>
+				<Button color="alternative">No, cancel</Button>
+			</div>
+		</Modal>
+	</form>
+</div>
