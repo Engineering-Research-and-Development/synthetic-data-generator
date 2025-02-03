8241f1c12458c7d2b9ddf168546b8bc6db672a94
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2025-02-03 16:55:11 +0100

    Refactoring tests

--- src/generator/test/app_test.py
+++ src/generator/test/app_test.py
@@ -3,6 +3,7 @@ import requests
 
 train_correct = "train_test.json"
 infer_correct_1 = "infer_test.json"
+infer_correct_2 = "infer_test_nodata.json"
 out_file = "test_results.txt"
 
 
@@ -17,6 +18,16 @@ def test_train():
 def test_infer():
     endpoint = "http://localhost:8010/infer"
     with open(infer_correct_1, 'r') as file:
+        data = json.load(file)
+    response = requests.post(endpoint, json=data)
+    assert response.status_code == 200
+    assert response.json()["result_data"] is not None
+    assert response.json()["metrics"] is not None
+
+
+def test_infer_nodata():
+    endpoint = "http://localhost:8010/infer"
+    with open(infer_correct_2, 'r') as file:
         data = json.load(file)
     response = requests.post(endpoint, json=data)
     assert response.status_code == 200
