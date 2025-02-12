2ff305945ea40e5c416ca0e33ab9450a2c3b605c
GiuseppeM <peppemartinelli66@gmail.com>
2025-02-12 12:51:59 +0100

    chore: updating only model_versions.py tests to see if the tests are working

--- src/model_registry/test/routers/model_versions.py
+++ src/model_registry/test/routers/model_versions.py
@@ -1,38 +1,28 @@
-import yaml
 import random
-from starlette.testclient import TestClient
-from src.model_registry.server.main import app
+from .conftest import server,port
 
-with open('src/model_registry/test/routers/config.yml', 'r') as file:
-    config = yaml.safe_load(file)
-    server = config["server"]
-    port = config['port']
+
 
 endpoint = "/versions"
 
-def test_get_all_versions():
-    with TestClient(app, client=(server, port)) as client:
-        response = client.get(server + endpoint)
-        print(response.json())
-        assert response.status_code == 200
-        random_version = random.choice(response.json())
-        assert random_version['id']
-        assert random_version['version_name']
-        assert random_version['image_path']
+def test_get_all_versions(client):
+    response = client.get(f"{server}:{port}{endpoint}")
+    assert response.status_code == 200
+    random_version = random.choice(response.json())
+    assert random_version['id']
+    assert random_version['version_name']
+    assert random_version['image_path']
 
+def test_get_version_id(client):
+    response = client.get(f"{server}:{port}{endpoint}" + "/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data['id']
+    assert data['version_name']
+    assert data['image_path']
 
-# def test_get_version_id():
-#     with TestClient(app, client=(server, port)) as client:
-#         response = client.get(server + endpoint + "/1")
-#         assert response.status_code == 200
-#         data = response.json()
-#         assert data['id']
-#         assert data['version_name']
-#         assert data['image_path']
-#
-# def test_get_bad_version_id():
-#     with TestClient(app, client=(server, port)) as client:
-#         assert client.get(server + endpoint + "/1000").status_code == 404
-#
+def test_get_bad_version_id(client):
+    assert client.get(f"{server}:{port}{endpoint}" + "/1000").status_code == 404
+
 # def test_create_version():
 #     pass
