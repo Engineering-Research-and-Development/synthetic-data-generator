465c0ca6cb92aa5296864f0ed18b5d35d32e15f9
Fabio Cassano <fabio.cassano@eng.it>
2025-03-27 14:39:10 +0100

    fixed tests

--- src/model_registry/test/routers/test_algorithm.py
+++ src/model_registry/test/routers/test_algorithm.py
@@ -1,5 +1,4 @@
 import requests
-from fastapi import status
 
 BASE_URL = "http://localhost:8001/algorithms"
 algo_id = None
@@ -16,7 +15,7 @@ def test_create_new_algorithm():
         "datatypes": [{"type": "float", "is_categorical": False}],
     }
     response = requests.post(BASE_URL, json=payload)
-    assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == 201
     assert "id" in response.json()
     algo_id = response.json()["id"]
 
@@ -24,18 +23,18 @@ def test_create_new_algorithm():
 def test_get_algorithm_by_id():
     algorithm_id = algo_id
     response = requests.get(f"{BASE_URL}/{algorithm_id}")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == 200
     assert "algorithm" in response.json()
     assert "datatypes" in response.json()
 
 
 def test_get_all_algorithms():
     response = requests.get(BASE_URL)
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == 200
     assert isinstance(response.json().get("algorithms"), list)
 
 
 def test_delete_algorithm():
     algorithm_id = algo_id
     response = requests.delete(f"{BASE_URL}/{algorithm_id}")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == 200
