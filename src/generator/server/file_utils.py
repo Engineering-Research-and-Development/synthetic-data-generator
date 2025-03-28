b77d903f7f00eaf99a6b9a211bcf73ce85320282
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2025-03-28 12:37:53 +0100

    fixing versions

--- src/generator/server/file_utils.py
+++ src/generator/server/file_utils.py
@@ -39,7 +39,8 @@ def check_latest_version(model_dir: Path | str):
     folder_path = TRAINED_MODELS / model_dir
     version = 0
     if check_folder(folder_path):
-        version = len([path for path in os.listdir(folder_path) if model_dir in path])
+        versions = [int(path.split("-")[-1].split("v")[0]) for path in os.listdir(folder_path) if model_dir in path]
+        version = max(versions)
     return version
 
 def list_trained_models():
