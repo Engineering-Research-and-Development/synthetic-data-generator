dc3da0c2a82c0b9274ec36218f1f847c794e78a0
Fabio Cassano <fabio.cassano@eng.it>
2025-04-08 13:50:38 +0200

    Added integration tests

--- src/integration_test/test/middleware/sdg_input.py
+++
@@ -1,6 +0,0 @@
-import json
-
-endpoint = "/sdg_input"
-
-features_payload = json.load(open("../../resources/sdg_input_with_features.json", "r"))
-file_payload = json.load(open("../../resources/sdg_input_with_file.json", "r"))
---
+++ src/integration_test/test/sdg_input.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import json
+import requests
+import pytest
+
+from conftest import middleware
+
+url = middleware + "/sdg_input/"
+
+def load_jsons():
+    resources_path = "../resources"
+    json_files = glob.glob(os.path.join(resources_path, "*.json"))
+
+    jsons = []
+    for json_file in json_files:
+        with open(json_file) as f:
+            jsons.append((json_file, json.load(f)))
+
+    return jsons
+
+@pytest.mark.parametrize("filename,payload", load_jsons())
+def test_sdg_input(filename, payload):
+    response = requests.post(url, json=payload)
+    assert response.status_code == 200, f"Failed on file: {filename} with status code {response.status_code} and body: {response.text}"
