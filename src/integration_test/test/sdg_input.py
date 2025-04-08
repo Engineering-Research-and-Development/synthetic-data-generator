908c5c9380657ab3c28e61a2018e1d7cb9842126
Fabio Cassano <fabio.cassano@eng.it>
2025-04-08 13:57:30 +0200

    Edited integration test pipeline

--- src/integration_test/test/sdg_input.py
+++ src/integration_test/test/sdg_input.py
@@ -8,6 +8,7 @@ from conftest import middleware
 
 url = middleware + "/sdg_input/"
 
+
 def load_jsons():
     resources_path = "../resources"
     json_files = glob.glob(os.path.join(resources_path, "*.json"))
@@ -19,7 +20,10 @@ def load_jsons():
 
     return jsons
 
+
 @pytest.mark.parametrize("filename,payload", load_jsons())
 def test_sdg_input(filename, payload):
     response = requests.post(url, json=payload)
-    assert response.status_code == 200, f"Failed on file: {filename} with status code {response.status_code} and body: {response.text}"
+    assert response.status_code == 200, (
+        f"Failed on file: {filename} with status code {response.status_code} and body: {response.text}"
+    )
