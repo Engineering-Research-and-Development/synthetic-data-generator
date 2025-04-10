b3aece78fb7223f0facf145ab5e2694f7a533284
Fabio Cassano <fabio.cassano@eng.it>
2025-04-10 15:12:10 +0200

    Edited integration

--- src/integration_test/test/connectivity.py
+++ src/integration_test/test/connectivity.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from conftest import middleware, couch, generator
 
+
 @pytest.mark.parametrize("url", [middleware, couch, generator])
 @pytest.mark.dependency()
 def test_services_connectivity(url):
--- src/integration_test/test/sdg_input.py
+++ src/integration_test/test/sdg_input.py
@@ -1,33 +1,31 @@
-import os
-import glob
-import json
+import time
+
 import requests
 import pytest
 
-from conftest import middleware
+from conftest import middleware, load_jsons
 from algorithms import algorithms_available
 
 url = middleware + "/sdg_input/"
 
 
-def load_jsons():
-    resources_path = "../resources"
-    json_files = glob.glob(os.path.join(resources_path, "*.json"))
-
-    jsons = []
-    for json_file in json_files:
-        with open(json_file) as f:
-            jsons.append((json_file, json.load(f)))
-
-    return jsons
-
-
-@pytest.mark.parametrize("filename,payload", load_jsons())
+@pytest.mark.dependency()
+@pytest.mark.parametrize("filename,payload", load_jsons("../resources/sdg_input"))
 def test_sdg_input(filename, payload, algorithms_available):
-    model_id = (list(algorithms_available.keys())[list(algorithms_available.values()).index(payload["test"])])
+    model_id = list(algorithms_available.keys())[
+        list(algorithms_available.values()).index(payload["test"])
+    ]
     payload.pop("test")
     payload["ai_model"]["selected_model_id"] = model_id
     response = requests.post(url, json=payload)
     assert response.status_code == 200, (
         f"Failed on file: {filename} with status code {response.status_code} and body: {response.text}"
     )
+
+
+@pytest.mark.dependency(name="test_sdg_input")
+def test_check_pushed_models():
+    time.sleep(10)
+    response = requests.get(middleware + "/trained_models")
+    json_response = response.json()
+    assert len(json_response["models"]) >= 1
