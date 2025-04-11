4683ec1d5da24cf4ad451e1b3a500c61d0bf71dd
netphantom <13152948+netphantom@users.noreply.github.com>
2025-04-11 14:37:04 +0000

    Apply Black formatting

--- src/integration_test/test/algorithms.py
+++ src/integration_test/test/algorithms.py
@@ -7,4 +7,4 @@ url = middleware + "/algorithms/"
 def get_algorithms_available():
     response = requests.get(url)
     algorithms = response.json()["algorithms"]
-    return  {item.get("id"): item.get("name") for item in algorithms}
+    return {item.get("id"): item.get("name") for item in algorithms}
