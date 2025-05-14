Fix time series VAE scaling, unscaling, latent dim

src/generator/ai_lib/data_generator/models/keras/implementation/TimeSeriesVAE.py
@@ -37,10 +37,10 @@ class TimeSeriesVAE(KerasBaseVAE):
         model_name: str,
         input_shape: str,
         load_path: str,
-        latent_dim: int = 6,
-        learning_rate: float = 3e-3,
+        latent_dim: int = 2,
+        learning_rate: float = 1e-3,
         batch_size: int = 16,
-        epochs: int = 100,
+        epochs: int = 60,
     ):
         super().__init__(metadata, model_name, input_shape, load_path, latent_dim)
         self._beta = 0.15
@@ -117,17 +117,20 @@ class TimeSeriesVAE(KerasBaseVAE):
         batch, feats, steps = data.shape
         if self._scaler is None:
             return data
-        return self._scaler.transform(data.reshape(-1, feats * steps)).reshape(
-            -1, feats, steps
-        )
+        data_reshaped = data.transpose(0,2,1).reshape(-1, feats)
+        data_scaled = self._scaler.transform(data_reshaped)
+        data_scaled = data_scaled.reshape(batch, steps, feats).transpose(0,2,1)
+        return data_scaled
 
     def _inverse_scale(self, data: np.array):
         if self._scaler is None:
             return data
         batch, feats, steps = data.shape
-        return self._scaler.inverse_transform(data.reshape(-1, feats * steps)).reshape(
-            -1, feats, steps
-        )
+        data_reshaped = data.transpose(0, 2, 1).reshape(-1, feats)
+        data_unscaled = self._scaler.inverse_transform(data_reshaped)
+        data_unscaled = data_unscaled.reshape(batch, steps, feats).transpose(0, 2, 1)
+        return data_unscaled
+
 
     def _pre_process(self, data: NumericDataset, **kwargs):
         np_data = np.array(data.dataframe.values.tolist())

src/generator/ai_lib/preprocess/scale.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np
 
 
@@ -25,7 +25,7 @@ def standardize_simple_tabular_input(
 
 def standardize_simple_tabular_time_series(
     train_data: np.array, test_data: np.array = None
-) -> tuple[StandardScaler, np.array, np.array]:
+) -> tuple[MinMaxScaler, np.array, np.array]:
     """
     Standardizes the time series data by scaling features to have zero mean and unit variance.
 
@@ -35,18 +35,17 @@ def standardize_simple_tabular_time_series(
              if provided.
     :raises DataException: If the input data does not have the expected shape.
     """
-    scaler = StandardScaler()
+    scaler = MinMaxScaler()
 
     batch, features, steps = train_data.shape
-    train_data = train_data.reshape(-1, features * steps)
 
-    train_data = scaler.fit_transform(train_data.reshape(-1, features * steps)).reshape(
-        -1, features, steps
-    )
+    x_reshaped = train_data.transpose(0, 2, 1).reshape(-1, features)
+    x_scaled = scaler.fit_transform(x_reshaped)
+    train_data = x_scaled.reshape(batch, steps, features).transpose(0, 2, 1)
 
     if test_data is not None:
-        test_data = scaler.transform(test_data.reshape(-1, features * steps)).reshape(
-            -1, features, steps
-        )
+        t_reshaped = test_data.transpose(0, 2, 1).reshape(-1, features)
+        t_scaled = scaler.transform(t_reshaped)
+        test_data = t_scaled.reshape(batch, steps, features).transpose(0, 2, 1)
 
     return scaler, train_data, test_data