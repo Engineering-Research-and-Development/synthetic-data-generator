27b93a47f8cc59940d961435ae75e645753c577f
emilio <emilio.cimino@outlook.it>
2025-06-25 12:22:59 +0200

    Add get_results method to function privatize compute method add _evaluate method
    Functions now both compute a result and evaluates

--- src/generator/ai_lib/functions/UnspecializedFunction.py
+++ src/generator/ai_lib/functions/UnspecializedFunction.py
@@ -1,6 +1,7 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+from ai_lib.functions.FunctionResult import FunctionResult
 from ai_lib.functions.Parameter import Parameter
 
 
@@ -9,13 +10,23 @@ class UnspecializedFunction(ABC):
         self.parameters = [Parameter.from_json(param) for param in parameters]
 
     @abstractmethod
-    def compute(self, data: np.array):
+    def _check_parameters(self):
         raise NotImplementedError
 
     @abstractmethod
-    def _check_parameters(self):
+    def _compute(self, data: np.array) -> tuple[np.array, np.array]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _evaluate(self, data: np.array) -> bool:
         raise NotImplementedError
 
     @classmethod
     def self_describe(cls):
         raise NotImplementedError
+
+    def get_results(self, data: np.array) -> dict:
+        results, indexes = self._compute(data)
+        evaluation_results = self._evaluate(data)
+        report = FunctionResult(results, indexes, evaluation_results)
+        return report.to_dict()
