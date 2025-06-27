87b6f4a445226d8c42f88a0844adf0b38e3ac258
emilio <emilio.cimino@outlook.it>
2025-06-27 08:55:28 +0200

    fix function info to match middleware

--- src/generator/ai_lib/post_process/functions/Parameter.py
+++ src/generator/ai_lib/post_process/functions/Parameter.py
@@ -1,5 +1,8 @@
+import builtins
+
+
 class Parameter:
-    def __init__(self, name, value, parameter_type):
+    def __init__(self, name:str, value:str, parameter_type:str):
         self.name = name
         self.value = value
         self.parameter_type = parameter_type
@@ -7,10 +10,14 @@ class Parameter:
     def to_json(self):
         return {
             "name": self.name,
-            "value": self.value,
+            "value": str(self.value),
             "parameter_type": self.parameter_type,
         }
 
     @classmethod
     def from_json(cls, json_data):
-        return cls(json_data["name"], json_data["value"], json_data["parameter_type"])
+        target_type = getattr(builtins, json_data["parameter_type"])
+        converted_value = target_type(json_data["value"])
+        return cls(json_data["name"],
+                   converted_value,
+                   json_data["parameter_type"])
