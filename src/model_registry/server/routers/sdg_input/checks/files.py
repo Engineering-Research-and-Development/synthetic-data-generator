9233f830373ad30ea7c874710ea3f05ebfcad7b2
Fabio Cassano <fabio.cassano@eng.it>
2025-07-07 16:41:09 +0200

    Bugfix on pydantic Model alignment

--- src/model_registry/server/routers/sdg_input/checks/files.py
+++ src/model_registry/server/routers/sdg_input/checks/files.py
@@ -84,7 +84,7 @@ def handle_user_file(
 
     return (
         GeneratorDataOutput(
-            functions=function_data,
+            functions_id=function_data,
             n_rows=data.get("additional_rows"),
             model=model,
             dataset=user_file,
